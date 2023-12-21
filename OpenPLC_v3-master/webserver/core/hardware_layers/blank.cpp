e35dc40c4275d936513e3c0c9807ff29305132e0
HesionBlack <1026320617@qq.com>
2023-12-21 23:38:12 +0800

    xx

--- OpenPLC_v3-master/webserver/core/hardware_layers/blank.cpp
+++ OpenPLC_v3-master/webserver/core/hardware_layers/blank.cpp
@@ -39,6 +39,8 @@
 #define MAX_ROWS 1024
 #define MAX_COLS 8
 unsigned char log_msg[1000];
+int sockfd = -1;//客户端socket通信句柄
+int tcp_client_connectflag = 0;//客户端socket通信连接标志
 // 还原稀疏数组为原始的二维数组
 void toOriginalArray(int sparse[][3], int original[][MAX_COLS]) {
     // 首先初始化原始数组为零
@@ -87,58 +89,84 @@ void toSparseArray(IEC_BOOL* original[][MAX_COLS], int rows, int cols, int spars
         }
     }
 }
-//-----------------------------------------------------------------------------
-// This function is called by the main OpenPLC routine when it is initializing.
-// Hardware initialization procedures should be here.
-//-----------------------------------------------------------------------------
-void initializeHardware()
-{
+void *wirteDataToPi(void *ptr){
+	pthread_mutex_lock(&bufferLock); //lock mutex
+	char* json_str;
+
+	if(tcp_client_connectflag==0){
+		return -1;
+	}
+	//往树莓派写数据
+	sprintf(log_msg, "往树莓派写数据...\n");
+	log(log_msg);	
+	int ret = 0;
+	char recvBuf[1024*8] = {0};
+	int recvBytes = 0;
+	cJSON *root = cJSON_CreateObject();
+    cJSON *matrix = cJSON_CreateArray();
+	cJSON_AddStringToObject(root,"run_model","W");
+	cJSON_AddNumberToObject(root,"int_output",*int_output[0]);
+	cJSON_AddItemToObject(root, "bool_input", matrix);
+	//bool_output 转成稀疏数组
+	// 计算非零元素数量
+    int nonZeroCount = countNonZeroElements(bool_output,1024,8);	
+	// 创建稀疏数组，行数为非零元素数量加1，列数固定为3
+    int sparse[nonZeroCount + 1][3];
+    // 填充稀疏数组
+    toSparseArray(bool_output, 1024, 8, sparse);	
+    for (int i = 0; i < nonZeroCount + 1; i++) {
+        cJSON *row = cJSON_CreateIntArray(sparse[i], 3);
+        cJSON_AddItemToArray(matrix, row);
+    }
+    json_str = cJSON_Print(root);
+	printf("正在发送\n");
+	if (0 > tcp_send(sockfd, json_str, strlen(json_str))) {//如果含有0x00不能用strlen
+		printf("发送失败...！\n");
+		tcp_client_connectflag = 0;
+	} else {
+		printf("发送成功\n");
+	}
+	//堵塞接收
+	memset(recvBuf, 0, sizeof(recvBuf));//清空
+	recvBytes = tcp_blocking_rcv(sockfd, recvBuf, sizeof(recvBuf));//堵塞接收
+	if (0 > recvBytes) {//接收失败
+		sprintf(log_msg, "接收失败\n");
+		log(log_msg);	
+	} else if (0 == recvBytes) {//断开了连接
+		sprintf(log_msg, "已断开连接\n");
+		log(log_msg);	
+	}else{
+		sprintf(log_msg, "收到信息:%s\n",recvBuf);
+		log(log_msg);	
+	}
+	pthread_mutex_unlock(&bufferLock); //unlock mutex
 }
 
-//-----------------------------------------------------------------------------
-// This function is called by the main OpenPLC routine when it is finalizing.
-// Resource clearing procedures should be here.
-//-----------------------------------------------------------------------------
-void finalizeHardware()
-{
-}
-
-//-----------------------------------------------------------------------------
-// This function is called by the OpenPLC in a loop. Here the internal buffers
-// must be updated to reflect the actual Input state. The mutex bufferLock
-// must be used to protect access to the buffers on a threaded environment.
-//-----------------------------------------------------------------------------
-void updateBuffersIn()
-{
+void *readPiData(void *ptr){
 	//从树莓派远程获取针脚数据
+
+	if(tcp_client_connectflag==0){
+		sprintf(log_msg, "未连接服务器...!\n");
+		log(log_msg);	
+		return -1;
+	}
+	sprintf(log_msg, "从树莓派远程获取针脚数据...!\n");
+	log(log_msg);		
 	pthread_mutex_lock(&bufferLock); //lock mutex
 	int ret = 0;
 	char recvBuf[1024*8] = {0};
 	int recvBytes = 0;
 	char* json_str;
-	pthread_t thread_client_rcv, thread_client_send;
-	int sockfd = -1;//客户端socket通信句柄
-	sockfd = tcp_creat_socket();//创建socket
-	if (0 > sockfd) {
-		sprintf(log_msg, "socket创建失败...!\n");
-		log(log_msg);		
-	}
-	printf("请求连接...\n");
-	if (0 > tcp_client_connect(sockfd, SERVER_ADDR, SERVER_PORT)) {
-		sprintf(log_msg, "连接失败....\n");
-		log(log_msg);				
-	} else {
-		sprintf(log_msg, "连接成功!\n");
-		log(log_msg);			
-	}	
-	//发送信息
-    sprintf(log_msg, "发送请求信息...!\n");
-	log(log_msg);		
 	cJSON *root = cJSON_CreateObject();
 	cJSON_AddStringToObject(root,"run_model","R");
 	json_str= cJSON_Print(root);
-	tcp_send(sockfd, json_str, strlen(json_str));
-
+	printf("正在发送\n");
+	if (0 > tcp_send(sockfd, json_str, strlen(json_str))) {//如果含有0x00不能用strlen
+		printf("发送失败...！\n");
+		tcp_client_connectflag = 0;
+	} else {
+		printf("发送成功\n");
+	}
 	//堵塞接收
 	memset(recvBuf, 0, sizeof(recvBuf));//清空
 	recvBytes = tcp_blocking_rcv(sockfd, recvBuf, sizeof(recvBuf));//堵塞接收
@@ -181,6 +209,76 @@ void updateBuffersIn()
 	}
 	pthread_mutex_unlock(&bufferLock); //unlock mutex
 }
+void *fun_client_connect(void *ptr){
+	while (1) {
+		if (0 == tcp_client_connectflag) {//未连接就不断中断重连
+	
+			if (sockfd > 0) {  //sockfd等于0时不能关，防止把文件句柄0关掉，导致scanf()函数输入不了
+				tcp_close(sockfd);
+			}
+			
+			sockfd = tcp_creat_socket();//创建socket
+			if (0 > sockfd) {
+				printf("socket创建失败...!\n");
+				sleep(2);
+				continue;
+			}
+			
+			printf("请求连接...\n");
+			if (0 > tcp_client_connect(sockfd, server_ip, server_port)) {
+				printf("连接失败...重连中...\n");
+				sleep(2);
+				continue;
+			} else {
+				tcp_client_connectflag = 1;
+				printf("连接成功!\n");
+			}	
+		} else {
+			sleep(1);
+		}
+	}
+	
+	tcp_close(sockfd);	
+}
+//-----------------------------------------------------------------------------
+// This function is called by the main OpenPLC routine when it is initializing.
+// Hardware initialization procedures should be here.
+//-----------------------------------------------------------------------------
+void initializeHardware()
+{
+	int ret = 0;
+	pthread_t thread_client_connect_holder;		
+	//创建一个维持连接的线程
+	ret = pthread_create(&thread_client_connect_holder, NULL, fun_client_connect, NULL);
+	if (ret < 0) {
+		printf("creat thread_client_rcv is fail!\n");
+		return -1;
+	}	
+}
+
+//-----------------------------------------------------------------------------
+// This function is called by the main OpenPLC routine when it is finalizing.
+// Resource clearing procedures should be here.
+//-----------------------------------------------------------------------------
+void finalizeHardware()
+{
+}
+
+//-----------------------------------------------------------------------------
+// This function is called by the OpenPLC in a loop. Here the internal buffers
+// must be updated to reflect the actual Input state. The mutex bufferLock
+// must be used to protect access to the buffers on a threaded environment.
+//-----------------------------------------------------------------------------
+void updateBuffersIn()
+{
+	pthread_t thread_client_read_pi;
+	int ret = 0;
+	ret = pthread_create(&thread_client_read_pi, NULL, readPiData, NULL);
+	if (ret < 0) {
+		printf("creat thread_client_rcv is fail!\n");
+		return -1;
+	}
+}
 
 //-----------------------------------------------------------------------------
 // This function is called by the OpenPLC in a loop. Here the internal buffers
@@ -189,61 +287,12 @@ void updateBuffersIn()
 //-----------------------------------------------------------------------------
 void updateBuffersOut()
 {
-	pthread_mutex_lock(&bufferLock); //lock mutex
-	char* json_str;
-	//往树莓派写数据
-	sprintf(log_msg, "往树莓派写数据...\n");
-	log(log_msg);		
+	pthread_t thread_client_write_data_to_pi;
 	int ret = 0;
-	char recvBuf[1024*8] = {0};
-	int recvBytes = 0;
-	pthread_t thread_client_rcv, thread_client_send;
-	int sockfd = -1;//客户端socket通信句柄
-	sockfd = tcp_creat_socket();//创建socket
-	if (0 > sockfd) {
-		sprintf(log_msg, "socket创建失败...!\n");
-		log(log_msg);		
+	ret = pthread_create(&thread_client_write_data_to_pi, NULL, wirteDataToPi, NULL);
+	if (ret < 0) {
+		printf("creat thread_client_rcv is fail!\n");
+		return -1;
 	}
-	printf("请求连接...\n");
-	if (0 > tcp_client_connect(sockfd, SERVER_ADDR, SERVER_PORT)) {
-		sprintf(log_msg, "连接失败....\n");
-		log(log_msg);				
-	} else {
-		sprintf(log_msg, "连接成功!\n");
-		log(log_msg);			
-	}	
-	cJSON *root = cJSON_CreateObject();
-    cJSON *matrix = cJSON_CreateArray();
-	cJSON_AddStringToObject(root,"run_model","W");
-	cJSON_AddNumberToObject(root,"int_output",*int_output[0]);
-	cJSON_AddItemToObject(root, "bool_input", matrix);
-	//bool_output 转成稀疏数组
-	// 计算非零元素数量
-    int nonZeroCount = countNonZeroElements(bool_output,1024,8);	
-	// 创建稀疏数组，行数为非零元素数量加1，列数固定为3
-    int sparse[nonZeroCount + 1][3];
-    // 填充稀疏数组
-    toSparseArray(bool_output, 1024, 8, sparse);	
-    for (int i = 0; i < nonZeroCount + 1; i++) {
-        cJSON *row = cJSON_CreateIntArray(sparse[i], 3);
-        cJSON_AddItemToArray(matrix, row);
-    }
-    json_str = cJSON_Print(root);
-	printf("%s",json_str);	
-	tcp_send(sockfd, json_str, strlen(json_str));
-	//堵塞接收
-	memset(recvBuf, 0, sizeof(recvBuf));//清空
-	recvBytes = tcp_blocking_rcv(sockfd, recvBuf, sizeof(recvBuf));//堵塞接收
-	if (0 > recvBytes) {//接收失败
-		sprintf(log_msg, "接收失败\n");
-		log(log_msg);	
-	} else if (0 == recvBytes) {//断开了连接
-		sprintf(log_msg, "已断开连接\n");
-		log(log_msg);	
-	}else{
-		sprintf(log_msg, "收到信息:%s\n",recvBuf);
-		log(log_msg);	
-	}
-	pthread_mutex_unlock(&bufferLock); //unlock mutex
 }
 
